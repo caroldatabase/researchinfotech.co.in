16933db9bb62133bf351e727c94e1b90f377625a
kundan <kundan.roy@motifworks.com>
2018-01-12 17:00:09 +0530

    payment

--- resources/views/investmentvia/paypal.blade.php
+++ resources/views/investmentvia/paypal.blade.php
@@ -100,28 +100,28 @@
                                 <td colspan="2"><b>Billing information(optional):</b></td>
                             </tr>
                             <tr>
-                                <td>Billing Name  :</td><td><input type="text" class="form-control"  name="billing_name" value="{{old('billing_name')}}"   placeholder="Enter Name" /></td>
+                                <td>Billing Name  :</td><td><input type="text" class="form-control"  name="billing_name" value="{{old('billing_name')}}"   placeholder="Enter Name" required="" /></td>
                             </tr>
                             <tr>
-                                <td>Billing Address :</td><td><input type="text" class="form-control"  name="billing_address" value="{{old('billing_address')}}" placeholder="Enter Address" /></td>
+                                <td>Billing Address :</td><td><input type="text" class="form-control"  name="billing_address" value="{{old('billing_address')}}" placeholder="Enter Address" required="" /></td>
                             </tr>
                             <tr>
-                                <td>Billing City  :</td><td><input type="text" class="form-control"  name="billing_city" value="{{old('billing_city')}}" placeholder="Enter City"  /></td>
+                                <td>Billing City  :</td><td><input type="text" class="form-control"  name="billing_city" value="{{old('billing_city')}}" placeholder="Enter City" required=""  /></td>
                             </tr>
                             <tr>
-                                <td>Billing State :</td><td><input type="text" class="form-control"  name="billing_state" value="{{old('billing_state')}}" placeholder="Enter State" /></td>
+                                <td>Billing State :</td><td><input type="text" class="form-control"  name="billing_state" value="{{old('billing_state')}}" placeholder="Enter State" required="" /></td>
                             </tr>
                             <tr>
-                                <td>Billing Zip :</td><td><input type="text" class="form-control"  name="billing_zip" value="{{old('billing_zip')}}" placeholder="Enter Zipcode"/></td>
+                                <td>Billing Zip :</td><td><input type="text" class="form-control"  name="billing_zip" value="{{old('billing_zip')}}" placeholder="Enter Zipcode" required="" /></td>
                             </tr>
                             <tr class="hide">
-                                <td>Billing Country :</td><td><input type="hidden" class="form-control"  name="billing_country" value="India"/></td>
+                                <td>Billing Country :</td><td><input type="hidden" class="form-control"  name="billing_country" value="India" /></td>
                             </tr>
                             <tr>
-                                <td>Billing Tel :</td><td><input type="text" class="form-control"  name="billing_tel" value="{{old('billing_tel')}}" placeholder="Enter Mobile number"  /></td>
+                                <td>Billing Tel :</td><td><input type="text" class="form-control" required=""  name="billing_tel" value="{{old('billing_tel')}}" placeholder="Enter Mobile number"  /></td>
                             </tr>
                             <tr>
-                                <td>Billing Email :</td><td><input type="text" class="form-control"  name="billing_email" value="test@test.com"/></td>
+                                <td>Billing Email :</td><td><input type="text" class="form-control"   name="billing_email" value="test@test.com"/></td>
                             </tr>
                             <tr>
                                 <td>Promo Code  :</td><td><input type="text" class="form-control"  name="promo_code" value="{{old('promo_code')}}"/></td>
