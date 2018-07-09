b159526b486242a2c1eed658fc88ca54d59de53c
programmerlab <kroy.iips@gmail.com>
2018-07-09 16:50:38 +0530

    servoces feature

--- modules/Admin/views/category/form.blade.php
+++ modules/Admin/views/category/form.blade.php
@@ -49,13 +49,19 @@
             </span>@endif
         </div>
     </div>  
-            
-            
-       <div class="form-group{{ $errors->first('feature', ' has-error') }}">
-        <label class="col-lg-2 col-md-2 control-label"> Service feature <span class="error">*</span></label>
+           
+
+     <div class="form-group{{ $errors->first('feature', ' has-error') }}">
+        <label class="col-lg-2 col-md-2 control-label">Service feature </label>
         <div class="col-lg-8 col-md-8"> 
-            {!! Form::text('feature',null, ['class' => 'form-control form-cascade-control input-small'])  !!} 
-            <span class="label label-danger">{{ $errors->first('feature', ':message') }}</span> 
+            {!! Form::textarea('feature',null, ['class' => 'form-control ckeditor form-cascade-control input-small'])  !!}
+            <span class="label label-danger">{{ $errors->first('feature', ':message') }}</span>
+            @if(Session::has('flash_alert_notice')) 
+            <span class="label label-danger">
+
+                {{ Session::get('flash_alert_notice') }} 
+
+            </span>@endif
         </div>
     </div> 
     
